4b6f96ddcdd2c59fe3425982f79287e05e4656e8
Usaid Mulla <Usaid@stargrp.com>
2025-05-22 10:00:23 +0530

    simple update

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from response_image import response_blueprint
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello, World! Flask app is live."
+
 # Configure upload settings
 configure_upload(app)
 
